02c47a6337317e44dac35ef8a292a92880bb8201
Leanne Zhang <lzhang800@gmail.com>
2019-01-01 14:32:39 -0800

    added readme

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,46 +1,48 @@
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter');
-const keys = require('./keys');
-const User = require('../models/user-model');
+const passport = require("passport");
+const TwitterStrategy = require("passport-twitter");
+const keys = require("./keys");
+const User = require("../models/user-model");
 
-// serialize the user.id to save in the cookie session 
+// serialize the user.id to save in the cookie session
 // so the browser will remember the user when login
 passport.serializeUser((user, done) => {
-    // parameters: error, user
-    done(null, user.id)
+  // parameters: error, user
+  done(null, user.id);
 });
 
 // deserialize the cookieUserId to user in the database
 passport.deserializeUser((id, done) => {
-    User.findById(id).then((user) => {
-        done(null, user)
-    })
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
-    new TwitterStrategy({
-        // options for the twitter start
-        consumerKey: keys.TWITTER_CONSUMER_KEY,
-        consumerSecret: keys.TWITTER_CONSUMER_SECRET,
-        callbackURL: '/auth/twitter/redirect'
-    }, async (token, tokenSecret, profile, done) => {
-        // find current user in UserModel
-        currentUser = await User.findOne({
-            twitterId: profile._json.id_str
-        });
-        // create new user if the database doesn't have this user
-        if (!currentUser) {
-            newUser = await new User({
-                name: profile._json.name,
-                screenName: profile._json.screen_name,
-                twitterId: profile._json.id_str,
-                profileImageUrl: profile._json.profile_image_url,
-            }).save();
-            if (newUser) {
-                return done(null, currentUser);
-            }
-        } 
-        return done(null, currentUser);       
-    })
-)
-
+  new TwitterStrategy(
+    {
+      // options for the twitter start
+      consumerKey: keys.TWITTER_CONSUMER_KEY,
+      consumerSecret: keys.TWITTER_CONSUMER_SECRET,
+      callbackURL: "/auth/twitter/redirect"
+    },
+    async (token, tokenSecret, profile, done) => {
+      // find current user in UserModel
+      const currentUser = await User.findOne({
+        twitterId: profile._json.id_str
+      });
+      // create new user if the database doesn't have this user
+      if (!currentUser) {
+        const newUser = await new User({
+          name: profile._json.name,
+          screenName: profile._json.screen_name,
+          twitterId: profile._json.id_str,
+          profileImageUrl: profile._json.profile_image_url
+        }).save();
+        if (newUser) {
+          return done(null, currentUser);
+        }
+      }
+      return done(null, currentUser);
+    }
+  )
+);
