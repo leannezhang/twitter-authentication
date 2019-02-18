0fdfaede1f5a25ca51d7d0db9de0cdbd8ea7783d
Leanne Zhang <lzhang800@gmail.com>
2019-02-17 17:49:02 -0800

    cleaned up and add image

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePage from "./components/Homepage";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./AppRouter.css";
 
 export const AppRouter = () => {
